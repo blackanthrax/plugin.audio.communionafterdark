a8d0ce524bb636e5b71d08345f46b853fd4f8c4f
blackanthrax <blackanthrax666@gmail.com>
2016-02-16 18:58:17 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Communion after Dark Radio Plugin
+
+## Requirements
+* KODI 15.x or later
+** tested with KODI 15.2
+* the KODI addon feedparser
+** which basically adds the feedparser python library
+** downloadable here: http://mirror.lstn.net/kodi/addons/gotham/script.module.feedparser/script.module.feedparser-5.1.3.zip
+
+## How to install 
+* Download the feedparser addon
+* Download the Communion after Dark addon (found at the releases tab)
+* put the files somewhere where you can find them (like the user's home folder)
+* Install them as zip file
+** SYSTEM > Settings > Add-Ons > install from zip file 
+
+Feedparser might be in the official repository of your KODI version and you might find it there
+
+## How to use
+* Go to Music > Plugins
+* Select Communion after Dark
+* wait for the tracklist to be loaded, this may take a while
+* play the set you favor
---
+++ default.py
@@ -0,0 +1,69 @@
+import sys
+import xbmc
+import xbmcgui
+import xbmcplugin
+import feedparser
+
+from urlparse import parse_qsl
+
+ADDON_ID="plugin.audio.communionafterdark"
+MEDIA_URL='special://home/addons/{0}/resources/media'.format(ADDON_ID)
+RSS_URI = "http://communionafterdark.com/rss.xml"
+
+_url = sys.argv[0]
+_handle = int(sys.argv[1])
+
+xbmcplugin.setContent(_handle, 'audio')
+
+def get_sets_from_rss():
+	xbmc.log("CAD Plugin:: GET FEEDS")
+	feed = feedparser.parse(RSS_URI)
+	return feed["items"]
+
+def get_sets():
+	sets = []
+	for (i, columns) in enumerate(get_sets_from_rss()):
+		link = columns.get("guid")
+		title = columns.get("title")
+		sets.append((title, link))
+	return sets
+
+def list_sets():
+	sets = get_sets()
+	listing = []
+
+	for (title, link) in sets:
+		list_item = xbmcgui.ListItem(label=title)
+		list_item.setProperty('IsPlayable', 'true')
+		list_item.setProperty('fanart_image', MEDIA_URL + "/fanart.jpg")
+
+		url = '{0}?action=play&set={1}'.format(_url, link)
+
+		is_folder = False
+
+		listing.append((url, list_item, is_folder))
+
+	xbmcplugin.addDirectoryItems(_handle, listing, len(listing))
+	xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
+	xbmcplugin.endOfDirectory(_handle)
+
+def play_set(path):
+	xbmc.log("CAD Plugin:: PLAY " + path)
+	play_item = xbmcgui.ListItem(path=path)
+	xbmcplugin.setResolvedUrl(_handle, True, listitem=play_item)
+
+def router(paramstring):
+	xbmc.log("CAD Plugin:: ROUTER")
+	params = dict(parse_qsl(paramstring))
+	xbmc.log("CAD Plugin:: " + paramstring)
+	if params:
+		if params['action'] == 'listing':
+			list_sets()
+		elif params['action'] == 'play':
+			play_set(params['set'])
+	else:
+		list_sets()
+
+if __name__ == '__main__':
+	xbmc.log("CAD Plugin:: MAIN THREAD")
+	router(sys.argv[2][1:])
